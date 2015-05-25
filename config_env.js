66f0b9ed564f96cb4a8da4c0274753783db5bd9a
Daniel Rudolph <daniel@bsolut.com>
2015-05-25 19:17:04 +0200

    default path is emtpy

--- config_env.js
+++ config_env.js
@@ -1,6 +1,6 @@
 var config = {
 	base_url: process.env.base_url || 'missing_base_url',
-	base_path: process.env.base_path || 'missing_path',
+	base_path: process.env.base_path || '',
 	youtube_api_key: process.env.youtube_api_key || 'missing_key',
 	port: process.env.PORT || 3000
 };
