fce7dafed9b1fe737c73274118ec659a497fab0b
Daniel Rudolph <daniel@bsolut.com>
2015-05-25 19:14:59 +0200

    some debug

--- config_env.js
+++ config_env.js
@@ -5,3 +5,7 @@ var config = {
 	port: process.env.PORT || 3000
 };
 exports.config = config;
+console.log("base_url: "+config.base_url);
+console.log("base_path: "+config.base_path);
+console.log("youtube_api_key: "+config.youtube_api_key);
+console.log("port: "+config.port);
