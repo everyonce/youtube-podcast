46664a62ba742a6647797d416dcab82fefc2a6e6
Daniel Rudolph <daniel@bsolut.com>
2015-05-25 18:37:29 +0200

    fix url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-	base_url: 'https://evening-bastion-3695.herokuapp.com/',
+	base_url: 'https://ypod.herokuapp.com/',
 	base_path: '',
 	youtube_api_key: 'AIzaSyCEf9ehLvWCwslE6PS58S8WDWaEq7AG2Ks',
 	port: 443
